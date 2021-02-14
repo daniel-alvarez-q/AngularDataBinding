338fcde4bbe5d73cb0608089b0cc4aa36e1a05a1
Daniel J Alvarez <daniel.alvarez.q@gmail.com>
2021-02-14 22:49:12 +0100

    Local reference and @ViewChild

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,30 +1,33 @@
-import { Component, OnInit, EventEmitter, Output} from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewEncapsulation, ViewChild, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.component.html',
-  styleUrls: ['./cockpit.component.css']
+  styleUrls: ['./cockpit.component.css'],
 })
 export class CockpitComponent implements OnInit {
 
-  newServerName = '';
-  newServerContent = ''; 
+  // newServerName = '';
+  // newServerContent = ''; 
 
   @Output() serverCreated = new EventEmitter <{name:string, content:string}> ();
 
   @Output('bpCreated') blueprintCreated = new EventEmitter <{name:string, content:string}> ();
+
+  @ViewChild ('serverContentInput', {static:true})serverContentInput: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onAddServer() {
-    this.serverCreated.emit({name: this.newServerName, content:  this.newServerContent });
+  onAddServer(input:HTMLInputElement) {
+    console.log(this.serverContentInput);
+    this.serverCreated.emit({name: input.value, content:  this.serverContentInput.nativeElement.value });  //Note the use of input. value for the referenced element (name)
   }
 
-  onAddBlueprint() {
-    this.blueprintCreated.emit({name: this.newServerName, content: this.newServerContent})
+  onAddBlueprint(input:HTMLInputElement) {
+    this.blueprintCreated.emit({name: input.value, content: this.serverContentInput.nativeElement.value})
   }
 
 }
